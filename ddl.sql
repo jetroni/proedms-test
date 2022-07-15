2c7589a621d437bf80043d54473e50711fa49fb4
proedms <>
2022-07-15 14:07:04 +0200

    Snapshot for catalog with Id: d902fd72-6184-46aa-a590-bd7f244f2a8f Snapshot Id: 895e9e4e-7ef2-49d7-9784-96039da5c866 Snapshot date: 2022-07-15T14:06:53.741+02:00

--- ddl.sql
+++ ddl.sql
@@ -8,19 +8,6 @@ create table `sakila`.`actor` (
   constraint `PRIMARY`
     primary key (`actor_id`)
 );
-create table `sakila`.`address` (
-  `address_id` smallint unsigned not null auto_increment,
-  `address` varchar(50) not null,
-  `address2` varchar(50) null,
-  `district` varchar(20) not null,
-  `city_id` smallint unsigned not null,
-  `postal_code` varchar(10) null,
-  `phone` varchar(20) not null,
-  `location` other null,
-  `last_update` timestamp not null default CURRENT_TIMESTAMP,
-  constraint `PRIMARY`
-    primary key (`address_id`)
-);
 create table `sakila_test`.`actor` (
   `actor_id` smallint unsigned not null auto_increment,
   `first_name` varchar(45) not null,
@@ -29,26 +16,7 @@ create table `sakila_test`.`actor` (
   constraint `PRIMARY`
     primary key (`actor_id`)
 );
-create table `sakila_test`.`address` (
-  `address_id` smallint unsigned not null auto_increment,
-  `address` varchar(50) not null,
-  `address2` varchar(50) null,
-  `district` varchar(20) not null,
-  `city_id` smallint unsigned not null,
-  `postal_code` varchar(10) null,
-  `phone` varchar(20) not null,
-  `location` other null,
-  `last_update` timestamp not null default CURRENT_TIMESTAMP,
-  constraint `PRIMARY`
-    primary key (`address_id`)
-);
 create unique index `PRIMARY` on `sakila`.`actor`(`actor_id` asc);
 create index `idx_actor_last_name` on `sakila`.`actor`(`last_name` asc);
-create unique index `PRIMARY` on `sakila`.`address`(`address_id` asc);
-create index `idx_fk_city_id` on `sakila`.`address`(`city_id` asc);
-create index `idx_location` on `sakila`.`address`(`location` asc);
 create unique index `PRIMARY` on `sakila_test`.`actor`(`actor_id` asc);
 create index `idx_actor_last_name` on `sakila_test`.`actor`(`last_name` asc);
-create unique index `PRIMARY` on `sakila_test`.`address`(`address_id` asc);
-create index `idx_fk_city_id` on `sakila_test`.`address`(`city_id` asc);
-create index `idx_location` on `sakila_test`.`address`(`location` asc);
